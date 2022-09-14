f159ad242acc4a213bc5609e0964ffd870480543
Josh T <josh@trejam.com>
2022-09-14 15:27:09 -0500

    cleanup

--- database_code.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy import create_engine
-from config import db_password
-db_string = f"postgresql://postgres:{db_password}@127.0.0.1:5432/AustinCrimeData"
-engine = create_engine(db_string)
